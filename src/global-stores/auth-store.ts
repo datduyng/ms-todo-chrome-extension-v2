94fdee73ff3b375886f9f07fc1892cfeabe69477
datduyng <datnguyen5653@gmail.com>
2021-08-11 20:08:43 -0700

    WIP: add authstore getter and setter

---
+++ src/global-stores/auth-store.ts
@@ -0,0 +1,37 @@
+const key = 'ms-auth-storage';
+let localStorage = chrome.extension.getBackgroundPage().localStorage;
+
+type AuthStore = {
+  version: string;
+  access_token: string | undefined;
+  refresh_token: string | undefined;
+  expires_in: number | undefined;
+  expires_at: Date | undefined;
+};
+
+const initState = { version: '0.0.0' };
+
+export const init = async () => {
+  localStorage[key] = JSON.stringify(initState);
+  return localStorage[key];
+};
+
+try {
+  if (
+    !localStorage[key] ||
+    JSON.parse(localStorage[key]).version !== initState.version
+  ) {
+    init();
+  }
+} catch (e) {
+  console.error('error on setting up auth store', e);
+  init();
+}
+
+export const get = () => {
+  return JSON.parse(localStorage[key] || '{}') as AuthStore;
+};
+
+export const set = (newState: AuthStore) => {
+  localStorage[key] = JSON.stringify(newState);
+};
