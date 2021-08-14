5f7ddcd302a6652e7a56e86bbd3750e5caa95e44
Samuel Bishop <sjbc35@mail.missouri.edu>
2021-08-13 17:52:07 -0700

    Style the login page. Allow for .png images to have correct file routing

--- src/pages/Popup/pages/login-page.tsx
+++ src/pages/Popup/pages/login-page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { firstTimeOauth2AndSaveToStore } from '../../../global-stores/auth-store';
+import microsoftLoginImage from "../../../assets/img/ms-symbollockup_signin_light_short.png"
+import './login-page.css'
 
 const Login = () => {
   return (
     <div>
-      <h1>Login page</h1>
-      <button
+      <h1 id="header">Login page</h1>
+      <button id="newtab"
         onClick={() => {
           chrome.windows.create({
             // Just use the full URL if you need to open an external page
@@ -15,13 +17,13 @@ const Login = () => {
       >
         New tab
       </button>
-      <button
-        onClick={() => {
+
+      <div id="login">
+        <img src={microsoftLoginImage} alt="MSFT Login" width="104" height="41" onClick={() => {
           firstTimeOauth2AndSaveToStore();
-        }}
-      >
-        Sign in with MS
-      </button>
+        }}></img>
+      </div>
+
     </div>
   );
 };
