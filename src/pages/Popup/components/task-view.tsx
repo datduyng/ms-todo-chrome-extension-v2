2dc7c5817e7dbbe251e7227b5a49c7002c1139fe
datduyng <datnguyen5653@gmail.com>
2021-08-29 11:05:24 -0700

    feat: UI for single task view

--- src/pages/Popup/components/task-view.tsx
+++ src/pages/Popup/components/task-view.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Checkbox } from '@atlaskit/checkbox';
 import StarFilledIcon from '@atlaskit/icon/glyph/star-filled';
+import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
+import CalendarIcon from '@atlaskit/icon/glyph/calendar';
 
 import Button from '@atlaskit/button';
 import { DateTimePicker } from '@atlaskit/datetime-picker';
@@ -12,28 +14,66 @@ const TaskView = () => {
   //   state.selectedFolderInfo,
   // ]);
   return (
-    <div style={{}}>
-      <div>
-        <DateTimePicker />
-        <Checkbox
-          isChecked={true}
-          onChange={() => {}}
-          name="checkbox-default"
-          testId="cb-default"
-        />
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          marginLeft: '10px',
+          marginRight: '10px',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ width: '10%' }}>
+          <Button
+            // iconAfter={<StarFilledIcon label="Star icon" size="small" />}
+            iconBefore={<ArrowLeftIcon label="arrow left icon" size="medium" />}
+            appearance="subtle"
+          ></Button>
+        </div>
+        <div style={{ width: '7%' }}>
+          <Checkbox
+            isChecked={true}
+            onChange={() => {}}
+            name="checkbox-default"
+            testId="cb-default"
+            size="large"
+          />
+        </div>
 
-        <Button
-          // iconAfter={<StarFilledIcon label="Star icon" size="small" />}
-          iconBefore={<StarFilledIcon label="Star icon" size="medium" />}
-          appearance="subtle"
-        >
-          
-        </Button>
+        <div
+          style={{
+            width: '2px',
+            marginLeft: '15px',
+            background: '#F0F0F0',
+            minHeight: '25px',
+          }}
+        ></div>
+        <div style={{width: '6%', paddingLeft: '10px'}}>
+          <CalendarIcon label="calendar icon" size="medium" primaryColor="gray"/>
+        </div>
+        <div style={{ width: '70%', paddingLeft: '6px', paddingRight: '5px' }}>
+          <DateTimePicker />
+        </div>
+        <div style={{ width: '10%' }}>
+          <Button
+            // iconAfter={<StarFilledIcon label="Star icon" size="small" />}
+            iconBefore={<StarFilledIcon label="Star icon" size="medium" />}
+            appearance="subtle"
+          ></Button>
+        </div>
       </div>
 
       <div>
-        <div>Name</div>
-        <div>Description</div>
+        <div contentEditable={true} id="myContentEditable" style={{
+          fontWeight: 'bold',
+          fontSize: '20px'
+        }}>name</div>
+        <div contentEditable={true} id="myContentEditable" style={{
+          fontSize: '13px'
+        }}>
+          Description
+          </div>
       </div>
     </div>
   );
